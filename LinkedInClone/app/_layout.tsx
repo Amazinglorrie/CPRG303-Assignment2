1040635d81a577962eb339aca075aa411e9d9bc8
Celine Thai <celine.thai@edu.sait.ca>
2026-02-07 00:16:48 -0700

    Refractoring code in progress
    pls don't look, it's messy rn LOL

--- LinkedInClone/app/_layout.tsx
+++ LinkedInClone/app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+<<<<<<< Updated upstream
   return <Stack />;
 }
+=======
+  
+  return (
+  <Stack screenOptions={{headerShown: false}} />
+  );
+}
+>>>>>>> Stashed changes
---
+++ LinkedInClone/components/landing-page.tsx
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, Image, TouchableOpacity } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { styles } from "./styleSheet";
+const LandingPage = () => {
+  return (
+    <LinearGradient
+      colors={["#0077B5", "#0A66C2", "#004182"]}
+      style={styles.containerLanding}
+    >
+      {/* LinkedIn Logo */}
+     
+      <Image
+        source={{
+          uri: "https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png",
+        }}
+        style={styles.logoLanding}
+      />
+      <Text style={styles.titleLanding}>Welcome to LinkedIn</Text>
+      <Text style={styles.subtitleLanding}>
+        Connect with professionals and grow your career
+      </Text>
+      <TouchableOpacity style={styles.buttonLanding}>
+        <Text style={styles.buttonTextLanding}>Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.buttonLanding, styles.secondaryButtonLanding]}>
+        <Text style={[styles.buttonTextLanding, styles.secondaryButtonTextLanding]}>
+          Sign Up
+        </Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  );
+};
+
+export default LandingPage;
