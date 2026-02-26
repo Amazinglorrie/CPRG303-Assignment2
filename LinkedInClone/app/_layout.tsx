2663fa6840691a75d8e25866b02c41ab79642003
LORETTA OKOH <amazinglorrie@gmail.com>
2026-02-26 01:15:16 -0700

    make all the change

--- LinkedInClone/app/_layout.tsx
+++ LinkedInClone/app/_layout.tsx
@@ -3,15 +3,5 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-<<<<<<< HEAD
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="landing-page" />
-      <Stack.Screen name="(tabs)" />
-    </Stack>
-  );
-=======
   return <Stack screenOptions={{headerShown: false}} />;
->>>>>>> ced1156decb840cb84ecbe04421fff3977f55857
 }
